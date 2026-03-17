d5b400946309bad0101a68040c9a9b51785ab9d8
iir20 <ismailratul340@gmail.com>
2026-03-18 05:29:36 +0600

    Delete worker.js

--- worker.js
+++
@@ -1,9 +0,0 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
-
-addEventListener('fetch', event => {
-  try {
-    event.respondWith(getAssetFromKV(event))
-  } catch (e) {
-    event.respondWith(new Response('Not Found', { status: 404 }))
-  }
-})
